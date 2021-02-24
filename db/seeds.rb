26f994e9023234fe59d48544ad6f258a0333c5e9
Yitzy Bratt <yitzybratt32@gmail.com>
2021-02-24 16:14:21 -0500

    creates movie model with title year and plot

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 # actor = Actor.new({ first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock" })
 # actor.save
-10.times do
-  actor = Actor.new(first_name: FFaker::Name.first_name, last_name: FFaker::Name.last_name, known_for: FFaker::Movie.title)
-  actor.save
-end
+# 10.times do
+#   actor = Actor.new(first_name: FFaker::Name.first_name, last_name: FFaker::Name.last_name, known_for: FFaker::Movie.title)
+#   actor.save
+# end
