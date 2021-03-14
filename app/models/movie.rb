f795b6482e9d6ab31aa8c42e30fcf88997c9f984
Yitzy Bratt <yitzybratt32@gmail.com>
2021-03-13 22:09:28 -0500

    adds Genre and GenreMovie models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,6 @@ class Movie < ApplicationRecord
   validates :plot, length: { minimum: 20, maximum: 500 }
   validates :year, numericality: { greater_than: 1900, less_than: 2022 }
   has_many :actors
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
 end
