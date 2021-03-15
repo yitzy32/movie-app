fc17202d680d9095d13d7a06a552607cf624cb8f
Yitzy Bratt <yitzybratt32@gmail.com>
2021-03-15 16:41:56 -0400

    adds movie_genres route and controller

---
+++ app/controllers/api/movie_genres_controller.rb
@@ -0,0 +1,10 @@
+class Api::MovieGenresController < ApplicationController
+  def create
+    @movie_genre = MovieGenre.new(
+      movie_id: params[:movie_id],
+      genre_id: params[:genre_id],
+    )
+    @movie_genre.save
+    render "show.json.jb"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -22,5 +22,7 @@ Rails.application.routes.draw do
     post "/users" => "users#create"
 
     post "/sessions" => "sessions#create"
+
+    post "/genres" => "movie_genres#create"
   end
 end
